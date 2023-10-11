08cf70ab2f4cafccd2c16a149ad3a1d6b5c73bc1
JastinXyz <jastinlinggar@gmail.com>
2023-10-11 17:05:49 +0700

    removing dark mode switch (now using media prefer color scheme only

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -37,28 +37,6 @@
 
             <x-footer />
         </div>
-
-        <div class="fixed top-10 right-10">
-            <x-button
-                type="button"
-                icon-only
-                variant="secondary"
-                sr-text="Toggle dark mode"
-                x-on:click="toggleTheme"
-            >
-                <x-heroicon-o-moon
-                    x-show="!isDarkMode"
-                    aria-hidden="true"
-                    class="w-6 h-6"
-                />
-
-                <x-heroicon-o-sun
-                    x-show="isDarkMode"
-                    aria-hidden="true"
-                    class="w-6 h-6"
-                />
-            </x-button>
-        </div>
     </div>
 </body>
 </html>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    darkMode: 'class',
+    darkMode: 'media',
 
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
