a9f97c0abe54628bdbc84a137eac4e42781fa489
JastinXyz <jastinlinggar@gmail.com>
2023-10-12 15:57:11 +0700

    change footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,19 +1,13 @@
 <footer class="flex-shrink-0 px-6 py-4">
     <p class="flex items-center justify-center gap-1 text-sm text-gray-600 dark:text-gray-400">
-        <span>Made with</span>
-
-        <span>
-            <x-heroicon-s-heart class="w-6 h-6 text-red-500" />
-        </span>
-
-        <span>by</span>
+        <span>&copy; {{ now()->year }}</span>
 
         <a
-            href="https://github.com/Kamona-WD"
+            href="https://github.com/affandra-solusi-teknologi"
             target="_blank"
-            class="text-blue-600 hover:underline"
+            class="text-zephyr-500 hover:underline"
         >
-            Ahmed Kamel
+            Affandra Solusi Teknologi
         </a>
     </p>
 </footer>
