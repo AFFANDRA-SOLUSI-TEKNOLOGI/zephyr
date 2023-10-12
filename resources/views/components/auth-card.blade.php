9fb625d056c6d2ee267eeea254326ec591f7fa20
JastinXyz <jastinlinggar@gmail.com>
2023-10-12 17:17:49 +0700

    change application logo

--- resources/views/components/auth-card.blade.php
+++ resources/views/components/auth-card.blade.php
@@ -1,7 +1,7 @@
 <main class="flex flex-col items-center flex-1 px-4 pt-6 sm:justify-center">
     <div>
         <a href="/">
-            <x-application-logo class="w-20 h-20" />
+            <x-application-logo />
         </a>
     </div>
 
--- resources/views/components/sidebar/header.blade.php
+++ resources/views/components/sidebar/header.blade.php
@@ -4,7 +4,7 @@
         href="{{ route('dashboard') }}"
         class="inline-flex items-center gap-2"
     >
-        <x-application-logo aria-hidden="true" class="w-10 h-auto" />
+        <x-application-logo />
 
         <span class="sr-only">Dashboard</span>
     </a>
